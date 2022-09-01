8ae44d521896539fc1ce60224904f87e12029677
Márcio Gabriel <37910437+KozielGPC@users.noreply.github.com>
2022-09-01 17:23:58 -0300

    OOPA: update calculator display

--- OOPA/Exercises/calculator.cpp
+++ OOPA/Exercises/calculator.cpp
@@ -1,46 +1,75 @@
 #include <iostream>
 
-enum Digit{
-    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+enum Digit
+{
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE
 };
 
-enum Signal{POSITIVE, NEGATIVE};
-
-class Display{
-    public:
-        void addDigit(Digit){
-            printf("Display\n");
-        };
-        void setSignal(Signal){};
-        void clear(){};
-};
-
-class Cpu{
-    public:
-        void receiveDigit(int);
-        void setDisplay(Display);
+enum Signal
+{
+    POSITIVE,
+    NEGATIVE
 };
 
 class Key;
 
-class Keyboard{
-    private:
-        void receiveDigit(int);
-        Key findKey(char);
-        void setCpu(Cpu);
-
+class Display
+{
+public:
+    void addDigit(Digit)
+    {
+        printf("Display\n");
+    };
+    void setSignal(Signal){};
+    void clear(){};
 };
 
-class Key{
-    public:
-        void press();
-        void setKeyboard(Keyboard);
+class Cpu
+{
+public:
+    void receiveDigit(int);
+    void setDisplay(Display);
+};
 
+class Keyboard
+{
+private:
+    void receiveDigit(int);
+    Key findKey(char);
+    void setCpu(Cpu);
+};
+
+class Key
+{
+public:
+    void press();
+    void setKeyboard(Keyboard);
+};
+
+class DisplayMarcio : public Display
+{
+public:
+    void addDigit(Digit digit)
+    {
+        Display::addDigit(digit);
+        printf("Display Marcio\n");
+    };
+    void setSignal(Signal){};
+    void clear(){};
 };
 
 int main()
 {
-    Display display;
+    DisplayMarcio display;
     display.addDigit(ONE);
     display.addDigit(ZERO);
     display.setSignal(NEGATIVE);
