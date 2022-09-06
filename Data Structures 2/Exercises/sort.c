176711e21ebd6f46fcb62667eb7ad77ad5efedd1
Márcio Gabriel <37910437+KozielGPC@users.noreply.github.com>
2022-09-05 22:30:44 -0300

    DE2:  add merge sort

--- Data Structures 2/Exercises/sort.c
+++ Data Structures 2/Exercises/sort.c
@@ -62,37 +62,44 @@ void recursiveInsertionSort(int *v, int n){
   }
 }
 
-void merge(int * v, int p, int q, int r){
-  int n1 = q - p + 1;
-  int n2 = r - q;
-  int * e = malloc(sizeof(int) * (n1 + 1));
-  int * d = malloc(sizeof(int) * (n2 + 1));
-  int i, j, k;
-  for(i = 0; i < n1; i++){
-    e[i] = v[p + 1];
-  }
-  e[n1] = INT_MAX;
-
-  for(j = 0; j < n1; j++){
-    d[j]= v[q+1+j];
-  }
-  d[n2] = INT_MAX;
-  i = 0;
-  j = 0;
-
-  for(k = p; k < r;k++){
-    if(e[i] <= d[j]){
-      v[k] = e[i];
-      i++;
+void merge(int *v, int p, int q, int r){
+    int n1 = q-p+1;
+    int n2 = r-q;
+    int *e = malloc(sizeof(int) * (n1+1));
+    int *d = malloc(sizeof(int) * (n2+1));
+    int i, j, k;
+    for(i = 0; i < n1; i++){
+        e[i] = v[p+i];
     }
-    else{
-      v[k] = d[j];
-      j++;
+    e[n1] = INT_MAX;
+    for(j = 0; j < n2; j++){
+        d[j] = v[q+1+j];
     }
-  }
+    d[n2] = INT_MAX;
+    i = 0;
+    j = 0;
+    for(k = p; k <= r; k++){
+        if(e[i] <= d[j]){
+            v[k] = e[i];
+            i++;
+        }
+        else{
+            v[k] = d[j];
+            j++;
+        }
+    }
 
   free(e);
   free(d);
+}
+
+void mergeSort(int * vetor, int e, int d){
+  if(e<d){
+    int m = (e+d)/2;
+    mergeSort(vetor, e, m);
+    mergeSort(vetor, m+1, d);
+    merge(vetor, e, m, d);
+  }
 }
 
 void imprimirVetor(int tamanho, int * vetor){
@@ -112,7 +119,7 @@ int main(int argc, char ** argv){
   imprimirVetor(n, v2);
 
   int v3[] = {3,5,6, 0, 4, 7};
-  merge(v3,0, 2, 5);
+  mergeSort(v3,0, 5);
   imprimirVetor(6, v3);
 }
 
