7c4511d63d5a3c24b36176b4fed9ce4021d74025
Márcio Gabriel <37910437+KozielGPC@users.noreply.github.com>
2022-09-08 17:16:55 -0300

    OOPA: update calculator

--- OOPA/Exercises/calculator.cpp
+++ OOPA/Exercises/calculator.cpp
@@ -25,9 +25,9 @@ class Key;
 class Display
 {
 public:
-    void addDigit(Digit)
+    void addDigit(Digit digit)
     {
-        printf("Display\n");
+        printf("Display lucio \n");
     };
     void setSignal(Signal){};
     void clear(){};
@@ -57,21 +57,75 @@ public:
 
 class DisplayMarcio : public Display
 {
+private:
+    std::string text;
+
 public:
     void addDigit(Digit digit)
     {
-        Display::addDigit(digit);
-        printf("Display Marcio\n");
+        switch (digit)
+        {
+        case ZERO:
+            setText(getText() + "0");
+            break;
+        case ONE:
+            setText(getText() + "1");
+            break;
+        case TWO:
+            setText(getText() + "2");
+            break;
+        case THREE:
+            setText(getText() + "3");
+            break;
+        case FOUR:
+            setText(getText() + "4");
+            break;
+        case FIVE:
+            setText(getText() + "5");
+            break;
+        case SIX:
+            setText(getText() + "6");
+            break;
+        case SEVEN:
+            setText(getText() + "7");
+            break;
+        case EIGHT:
+            setText(getText() + "8");
+            break;
+        case NINE:
+            setText(getText() + "9");
+            break;
+
+        default:
+            setText(getText() + " ?");
+            break;
+            break;
+        };
+        this->showText();
     };
     void setSignal(Signal){};
     void clear(){};
+
+    void setText(std::string text)
+    {
+        this->text = text;
+    }
+
+    std::string getText()
+    {
+        return this->text;
+    }
+    void showText()
+    {
+        printf("%s\n", this->getText());
+    }
 };
 
 int main()
 {
     DisplayMarcio display;
     display.addDigit(ONE);
-    display.addDigit(ZERO);
-    display.setSignal(NEGATIVE);
+    // display.addDigit(ZERO);
+    // display.setSignal(NEGATIVE);
     display.clear();
 }
